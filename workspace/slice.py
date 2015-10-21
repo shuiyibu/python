5b559abf38466fae10f65beabb6c582d886b4dc2
Dylan <shuiyibu.ja@gmail.com>
2015-10-21 22:24:01 +0800

    tt

---
+++ workspace/slice.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+#_*_ coding _*_
+
+# L = ['Michael','Sarah','Tracy','Bob','Jack']
+L=['Michael','Sarah','Tracy','Bob','Jack']
+r=[]
+n=3
+for i in range(n):
+    r.append(L[i])
+print(r)
+
+print(L[-1])
+print(L[-2:])
+L2=list(range(100))
+#print(L2)
+print(L2[:10:2])
+str='asdfgh'
+print(str[:3])
+print(str[3:])
